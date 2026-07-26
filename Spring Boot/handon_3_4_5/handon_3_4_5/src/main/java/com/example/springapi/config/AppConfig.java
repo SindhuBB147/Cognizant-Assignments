2ceb097bf41f3d8e74adbb58202611e2d3e5f957
Sindhu B Binkadakatti <YOUR_GITHUB_EMAIL>
2026-07-26 12:14:38 +0530

    Completed remaining Cognizant assignments

--- Spring Boot/handon_3_4_5/handon_3_4_5/src/main/java/com/example/springapi/config/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.springapi.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-
-@Configuration
-@ImportResource({"classpath:country.xml", "classpath:employee.xml"})
-public class AppConfig {
-}
